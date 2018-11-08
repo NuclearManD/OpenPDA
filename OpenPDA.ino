5cdb06f6a5864b8ffa0d32e3674f4e6d78bfd0a5
NuclearManD <dylanbrophy@gmail.com>
2018-11-08 10:41:53 -0800

    work

--- OpenPDA.ino
+++ OpenPDA.ino
@@ -1,22 +1,49 @@
 #include <TouchScreen.h>
+#include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_HX8357.h"
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
 
-#include <Adafruit_HX8357.h>
-
-#define CS 21
-#define DC 36
+// These are 'flexible' lines that can be changed
+#define TFT_CS 21
+#define TFT_DC 4
+#define TFT_RST 14
 #define YPLUS A3
 #define XPLUS A2
 #define YMINS A1
 #define XMINS A0
 
-void setup() {
-  // put your setup code here, to run once:
+// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+//Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
 
+// SoftSPI - note that on some processors this might be *faster* than hardware SPI!
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, 18, 5, TFT_RST, 19);
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("HX8357D Test!"); 
+
+  //SPI.setClockDivider(SPI_CLOCK_DIV);
+  tft.begin(HX8357D);
+
+  // read diagnostics (optional but can help debug problems)
+  uint8_t x = tft.readcommand8(HX8357_RDPOWMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDCOLMOD);
+  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDIM);
+  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDSDR);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
+  
+  Serial.println(F("Benchmark                Time (microseconds)"));
+
+  tft.setRotation(1);
+  tft.println("Hello World!!");
 }
 
 void loop() {
