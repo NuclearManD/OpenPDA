117d36253a9a155114d56b6b22a4069dc5f643aa
NuclearManD <dylanbrophy@gmail.com>
2018-11-08 14:40:32 -0800

    upd wiring

--- OpenPDA.ino
+++ OpenPDA.ino
@@ -8,10 +8,17 @@
 #define TFT_CS 21
 #define TFT_DC 4
 #define TFT_RST 14
-#define YPLUS A3
-#define XPLUS A2
-#define YMINS A1
-#define XMINS A0
+
+#define YP 33  // must be an analog pin, use "An" notation!
+#define XM 13  // must be an analog pin, use "An" notation!
+#define YM 12   // can be a digital pin
+#define XP 27   // can be a digital pin
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 148
+#define TS_MINY 297
+#define TS_MAXX 930
+#define TS_MAXY 960
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 //Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
@@ -23,30 +30,13 @@ Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, 18, 5, TFT_RST, 19);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("HX8357D Test!"); 
-
-  //SPI.setClockDivider(SPI_CLOCK_DIV);
   tft.begin(HX8357D);
 
-  // read diagnostics (optional but can help debug problems)
-  uint8_t x = tft.readcommand8(HX8357_RDPOWMODE);
-  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(HX8357_RDMADCTL);
-  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(HX8357_RDCOLMOD);
-  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(HX8357_RDDIM);
-  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(HX8357_RDDSDR);
-  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
-  
-  Serial.println(F("Benchmark                Time (microseconds)"));
-
-  tft.setRotation(1);
+  tft.fillScreen(0);
+  tft.setRotation(0);
   tft.println("Hello World!!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
