e515969a3f1196fff1638f472d4776e50efb7827
NuclearManD <dylanbrophy@gmail.com>
2018-11-08 10:39:54 -0800

    OS indev

---
+++ OpenPDA.ino
@@ -0,0 +1,25 @@
+#include <TouchScreen.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Adafruit_HX8357.h>
+
+#define CS 21
+#define DC 36
+#define YPLUS A3
+#define XPLUS A2
+#define YMINS A1
+#define XMINS A0
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
