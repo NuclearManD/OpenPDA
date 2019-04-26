2c4928739387ec31b77df79687ff1865ca9f59ab
NuclearManD <dylanbrophy@gmail.com>
2019-04-25 20:43:31 -0700

    much, MUCH better firmware.

--- OpenPDA.ino
+++ OpenPDA.ino
@@ -1,11 +1,14 @@
 #include <TouchScreen.h>
 #include <SPI.h>
+#include <TinyGPS.h>
 #include "WiFi.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_HX8357.h"
 
 #define LEDC_CHANNEL_0 0
 #define LEDC_CHANNEL_1 1
+
+#define LASER_PIN 22
 
 #include "tv_b_gone.h"
 
@@ -41,17 +44,33 @@
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, MOSI, SCK, TFT_RST, MISO);
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
-boolean redraw = false;
-boolean screenDirty = false;
-int state = -1;
-
 long idleTime;
+TinyGPS gps;
+
+String logfile = "";
+
+void println(String s){
+  println(s);
+  logfile+=s+'\n';
+}
+void print(String s){
+  print(s);
+  logfile+=s;
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Setting up...");
+  Serial.begin(9600);
+  println("Setting up...");
+  
+  tft.begin(HX8357D);
+
+  tft.setRotation(2);
+  drawIcon(0,0,5);
+  
+  println("TFT Configured...");
 
   pinMode(IRLED, OUTPUT);
+  pinMode(LASER_PIN, OUTPUT);
 
   SPI.setFrequency(8000000);
   
@@ -61,7 +80,7 @@ void setup() {
   ledcSetup(5, 80, 13);
   ledcAttachPin(IRLED, 5);
   
-  Serial.println("LEDC Configured...");
+  println("LEDC Configured...");
   
   setBrightness(30);
   
@@ -70,190 +89,152 @@ void setup() {
   analogSetClockDiv(8);
   analogSetAttenuation(ADC_11db);
   
-  Serial.println("ADC Configured...");
-  
-  tft.begin(HX8357D);
-
-  tft.fillScreen(0);
-  tft.setRotation(1);
-  
-  Serial.println("TFT Configured...");
-
-  //mkTask(guiProcessFunc, "GUI Receiver", &guiPgm, 8192);
-
-  redraw = true;
-  state  = -1;
+  println("ADC Configured...");
 
   idleTime=millis()+180*1000;
-  Serial.println("Ready!");
-}
-#define STATE_LOCKED -1
-#define STATE_HOME 0
-#define STATE_STORAGE 1
-#define STATE_NETWORK 2
-#define STATE_SETTINGS 3
+  delay(500);
+  tft.fillScreen(WHITE);
+  println("Ready!");
 
-int numStorageDevices = 0;
-void loop() {
-  if(idleTime<millis()){
-    ledcWrite(1, 0);
-    while(true){
-      delay(20);
-      if(getPoint().z!=0)break;
-    }
-    setBrightness(getBrightness());
-    delay(100);
-    idleTime=millis()+180*1000; // three minutes
-  }
-  if(redraw){
-    if(state==STATE_LOCKED){
-      state=STATE_HOME;
-    }
-    if(state==STATE_HOME){
-      drawHome();
-    }else if(state==STATE_STORAGE){
-      tft.setTextColor(WHITE);
-      tft.fillRoundRect(15,15,tft.width()-45,180,5,BLUE);
-      tft.setCursor(35,35);
-      tft.setTextSize(2);
-      if(numStorageDevices==0){
-        tft.print("No storage devices.");
-      }else{
-        
+  drawIcon(0,0,5);
+  // login
+  while(true){
+    if(idleTime<millis()){
+      ledcWrite(1, 0);
+      while(true){
+        delay(20);
+        if(getPoint().z!=0)break;
       }
-      tft.fillRect(30,150,60,20, WHITE);
-      tft.setTextColor(BLUE);
-      tft.setCursor(32,155);
-      tft.print("HOME");
-    }else if(state==STATE_NETWORK){
-      tft.setTextColor(WHITE);
-      tft.fillRoundRect(15,15,tft.width()-45,180,5,RED);
-      tft.setCursor(35,35);
-      tft.setTextSize(1);
-      
-      tft.print("[networking disabled]");
-      
-      tft.setCursor(250, 35);
-      tft.print("Error: networking unavailable.");//"Scanning for networks...");
-      /*
-      WiFi.mode(WIFI_STA);
-      WiFi.disconnect();
+      setBrightness(getBrightness());
       delay(100);
-      int numNetworks = WiFi.scanNetworks();
-      delay(20);
-      
-      tft.setCursor(250, 35);
-      tft.fillRect(250,35,100,30, RED);
-      
-      if(numNetworks==0){
-        tft.print("No APs!");
-      }else{
-        for(int i=0;i<numNetworks;i++){
-          tft.setCursor(250, 35+i*20);
-          tft.print(WiFi.SSID(i));
-          tft.print(" : ");
-          tft.print(WiFi.RSSI(i));
+      idleTime=millis()+180*1000; // three minutes
+    }
+    TSPoint p;
+    if((p=getPoint()).z==0)continue;
+    idleTime=millis()+180*1000; // three minutes
+    if(p.x<30&&p.y<30){
+      break;
+    }
+    delay(5);
+  }
+}
+
+boolean laser = false;
+
+void loop() {
+  // draw OS stuff
+  tft.fillScreen(BLACK);
+  drawIcon(25,25,4);
+  tft.drawRect(0,0,100,100,WHITE);
+  tft.setCursor(125,25);
+  tft.setTextSize(2);
+  tft.setTextColor(RED);
+  tft.print("LASER");
+  tft.drawRect(100,0,100,100,WHITE);
+  tft.setCursor(125,125);
+  tft.setTextSize(2);
+  tft.setTextColor(WHITE);
+  tft.print("OFF");
+  tft.drawRect(100,100,100,100,WHITE);
+  drawIcon(225,125,6);
+  drawIcon(25,125,7);
+  tft.drawRect(0,100,100,100,WHITE);
+  if(laser){
+    // fill red triangle for laser at (220, 10)
+    tft.fillTriangle(220,10,228, 22, 212, 22, RED);
+  }else{
+    // draw a red triangle for no laser at (220, 10)
+    tft.drawTriangle(220,10,228, 22, 212, 22, RED);
+  }
+
+  while(true){
+    if(idleTime<millis()){
+      ledcWrite(1, 0);
+      while(true){
+        delay(20);
+        if(getPoint().z!=0)break;
+      }
+      setBrightness(getBrightness());
+      delay(100);
+      idleTime=millis()+180*1000; // three minutes
+    }else{
+      // only process GPS if not off/idle
+      while (Serial.available()){
+        char c = Serial.read();
+        // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+        if (gps.encode(c)){ // Did a new valid sentence come in?
+          int year;
+          byte month, day, hour, minute, second, hundredths;
+          gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
+          tft.setCursor(225,50);
+          tft.setTextSize(1);
+          tft.setTextColor(WHITE);
+          tft.print(hour);
+          tft.print(':');
+          tft.print(minute);
         }
       }
-      WiFi.mode(WIFI_OFF);
-      btStop();
-      delay(100);
-      */
-      
-      tft.fillRect(30,150,60,20, WHITE);
-      tft.setTextColor(RED);
-      tft.setCursor(32,155);
-      tft.print("HOME");
-    }else if(state==STATE_SETTINGS){
-      tft.setTextColor(WHITE);
-      tft.fillRoundRect(15,15,tft.width()-45,180,5,GREY2);
-      
-      tft.setCursor(35,35);
-      tft.setTextSize(2);
-      tft.print("Brightness");
-
-      tft.drawLine(40,70,105,70,WHITE);
-      tft.drawLine(getBrightness()+40,60,getBrightness()+40,80,WHITE);
-      
-      tft.fillRect(30,150,60,20, WHITE);
-      tft.setTextColor(GREY2);
-      tft.setCursor(32,155);
-      tft.print("HOME");
     }
-    redraw = false;
-  }else{
-    delay(20);
-  }
-  TSPoint p;
-  if((p=getPoint()).z==0)return;
-  idleTime=millis()+180*1000; // three minutes
-  /*Serial.print(p.x);
-  Serial.print(" ");
-  Serial.println(p.y);//*/
-  tft.drawCircle(p.x,p.y,10,WHITE);
-  if(state==STATE_HOME){
-    if(p.x<170){
-      redraw=true;
-      if(p.y<60){ // storage selected
-        state=STATE_STORAGE;
-        Serial.println("TV-B-GONE test...");
-        long timer = millis();
-        tv_b_gone();
-        Serial.println(millis()-timer);
-      }else if(p.y<110){ // network selected
-        state=STATE_NETWORK;
-      }else if(p.y<170){ // network selected
-        state=STATE_SETTINGS;
+    TSPoint p;
+    if((p=getPoint()).z==0)continue;
+    idleTime=millis()+180*1000; // three minutes
+    if(p.x<100&&p.y<100){
+      tft.fillRect(100,100,120,60, BLUE);
+      tft.setCursor(110,110);
+      tft.setTextSize(1);
+      tft.setTextColor(WHITE);
+      tft.print("Running...");
+      tv_b_gone();
+      return;
+    }else if(p.x>100&&p.x<200&&p.y<100){
+      laser = !laser;
+      digitalWrite(LASER_PIN, laser);
+
+      // only redraw icon and stay in input loop (avoid visual disturbance of screen clear)
+      if(laser){
+        // fill red triangle for laser at (220, 10)
+        tft.fillTriangle(220,10,228, 22, 212, 22, RED);
+      }else{
+        // drawa red triangle for no laser at (220, 10)
+        tft.fillTriangle(220,10,228, 22, 212, 22, BLACK);
+        tft.drawTriangle(220,10,228, 22, 212, 22, RED);
+      }
+      delay(50);
+    }else if(p.x>100&&p.x<200&&p.y>100&&p.y<200){
+      idleTime=0; // turn off the display
+      ledcWrite(1, 0);
+      delay(250);
+    }else if(p.x<100&&p.y>100&&p.y<200){
+      // open log file
+      tft.fillScreen(BLACK);
+      tft.setTextSize(1);
+      tft.setTextColor(GREEN);
+      tft.print(logfile);
+      delay(200);
+      while(true){
+        if(idleTime<millis()){
+          ledcWrite(1, 0);
+          while(true){
+            delay(20);
+            if(getPoint().z!=0)break;
+          }
+          setBrightness(getBrightness());
+          delay(100);
+          idleTime=millis()+180*1000; // three minutes
+        }
+        TSPoint p;
+        if((p=getPoint()).z==0)continue;
+        idleTime=millis()+180*1000; // three minutes
+        break;
       }
     }
-  }else if(state==STATE_STORAGE){
-    if((p.x>30)&&(p.y>150)&&(p.x<90)&&(p.y<170)){
-      state=STATE_HOME;
-      redraw=true;
-      screenDirty=true;
-    }
-  }else if(state==STATE_NETWORK){
-    if((p.x>30)&&(p.y>150)&&(p.x<90)&&(p.y<170)){
-      state=STATE_HOME;
-      redraw=true;
-      screenDirty=true;
-    }
-  }else if(state==STATE_SETTINGS){
-    if((p.x>30)&&(p.y>150)&&(p.x<90)&&(p.y<170)){
-      state=STATE_HOME;
-      redraw=true;
-      screenDirty=true;
-    }
-    if((p.x>40)&&(p.x<105)&&(p.y>60)&&(p.y<80)){
-      tft.fillRect(40,60,100,21,GREY2);
-      tft.drawLine(40,70,105,70,WHITE);
-      tft.drawLine(getBrightness()+40,60,getBrightness()+40,80,WHITE);
-      setBrightness(p.x-40);
-    }
   }
+  
+
+  // process a press (from main menu)
 }
 
 #define ICONSIZE 50
-
-int numPgms = 3;
-char* pgmNames[] = {"Storage","Network","Settings"};
-int   pgmIcons[] = {2,1,3};
-void drawHome(){
-  if(screenDirty){
-    tft.fillScreen(0);
-    screenDirty=false;
-  }
-  tft.setTextColor(WHITE);
-  // draw background
-  //  (none yet)
-  // draw icons
-  tft.setTextSize(3);
-  for(int i=0;i<numPgms;i++){
-    drawIcon(10, i*(ICONSIZE+10)+5, pgmIcons[i]);
-    tft.setCursor(20+ICONSIZE, i*(ICONSIZE+10) + 10);
-    tft.print(pgmNames[i]);
-  }
-}
 void drawIcon(int x, int y, int id){
   if(id==0){ // hard disk icon
     tft.drawRect(x,y+5,50,40,CYAN);
@@ -282,6 +263,39 @@ void drawIcon(int x, int y, int id){
     tft.fillRect(x+20,y,10,50,GREY1);
     tft.fillRect(x,y+20,50,10,GREY1);
     tft.fillCircle(x+25,y+25,4,GREY2);
+  }else if(id==4){  // tv-b-gone
+    tft.fillRect(x+5,y+10,40,25,BLACK);
+    tft.drawRect(x+5,y+10,40,25,GREY1);
+    tft.drawLine(x+10,y+10,x+4,y+4,GREY2);
+    tft.drawLine(x+40,y+10,x+46,y+4,GREY2);
+    tft.drawLine(x+7,y+12,x+42,y+32,RED);
+    tft.drawLine(x+42,y+12,x+7,y+32,RED);
+    tft.drawRect(x+8,y+36,34,2,GREY2);
+  }else if(id==5){  // scaa logo (POSITION INDEPENDEDNT!)
+    int w = tft.width();
+    int h = tft.height();
+    tft.fillCircle(w/2,h/2,w/5, GREY1);
+    tft.drawCircle(w/2,h/2,w/5, RED);
+    tft.setTextSize(3);
+    tft.setCursor(w/2 - 40,h/2 - 10);
+    tft.setTextColor(BLUE);
+    tft.print("SCAA");
+  }else if(id==6){  // scaa logo (POSITION DEPENDEDNT!)
+    int q = ICONSIZE/2;
+    tft.fillCircle(x+q,y+q,q, GREY1);
+    tft.drawCircle(x+q,y+q,q, RED);
+    tft.setTextSize(1);
+    tft.setCursor(x+q - 20,y+q - 5);
+    tft.setTextColor(BLUE);
+    tft.print("SCAA");
+  }else if(id==7){  // logfile
+    int q = ICONSIZE/2;
+    tft.fillRect(x+5,y+5,40,40,BLACK);
+    tft.drawRect(x+5,y+5,40,40,GREY2);
+    tft.setCursor(x+7,y+7);
+    tft.setTextSize(1);
+    tft.setTextColor(GREEN);
+    tft.print("> LOG");
   }
 }
 TSPoint getPoint(){
@@ -290,23 +304,23 @@ TSPoint getPoint(){
     p.z=0;
     return p;
   } //*/
-  /*Serial.print(p.x);
-  Serial.print(" ");
-  Serial.print(p.y);
-  Serial.print(" ");
-  Serial.println(p.z);
+  /*print(p.x);
+  print(" ");
+  print(p.y);
+  print(" ");
+  println(p.z);
   delay(100);         //*/
   int y=p.y;
-  p.y = tft.height()-map(p.x, TS_MINX, TS_MAXX, 0, tft.height());
-  p.x = map(y, TS_MINY, TS_MAXY, 0, tft.width());
+  p.x = tft.width()-map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+  p.y = tft.height()-map(y, TS_MINY, TS_MAXY, 0, tft.height());
   if(p.z==0)p.z=1;
   if(p.x<0||p.y<0||p.x>tft.width()||p.y>tft.height())p.z=0;
   else{
-    /*Serial.print(p.x);
-    Serial.print(" ");
-    Serial.print(p.y);
-    Serial.print(" ");
-    Serial.println(p.z);
+    /*print(p.x);
+    print(" ");
+    print(p.y);
+    print(" ");
+    println(p.z);
     delay(100);         //*/
   }
   return p;
